8e6d968ab13c1f16ac45b9daeb1eb0fe5b293ec6
Zeynep Emine Yalçın <zeyalcin@stu.okan.edu.tr>
2023-11-16 21:44:32 +0300

    CalculatorApiConsoleClient Added

---
+++ CalculatorApiConsoleClient/CalculatorApiConsoleClient/CalculationRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorApiConsoleClient
+{
+    internal class CalculationRequest
+    {
+        public CalculationRequest(string methodName, double num1, double num2)
+        {
+            MethodName = methodName;
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        public string MethodName { get; set; }
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public double Result { get; set; }
+
+    }
+
+}
